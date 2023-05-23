36615f340032341e7b01bd46bf0c2f5437d22203
TechlabRS <129305203+TechlabRS@users.noreply.github.com>
2023-05-23 20:55:43 +0530

    Update rabbitmq.sh

--- rabbitmq.sh
+++ rabbitmq.sh
@@ -17,7 +17,8 @@ curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/sc
 func_stat_check $?
 
 func_print_head "Install ErLang & RabbitMQ"
-yum install rabbitmq-server -y &>>$log_file --removed erlang its producing error
+#removed erlang its producing error
+yum install rabbitmq-server -y &>>$log_file
 func_stat_check $?
 
 func_print_head "Start RabbitMQ Service"
